d92a59c353098350dd27f828ef68163b88e2408c
tkhwang <tkhwang@gmail.com>
2018-12-28 11:28:48 +0900

    Fix client/build path after refactoring

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ config = {
     operatorsAliases: 'false',
     jwtPrivateKey: process.env.JWT_KEY,
     mongodb: 'mongodb://localhost/joy',
-    STATIC_FILES: '../client-im09-prj-joy/build'
+    STATIC_FILES: '/../../client-im09-prj-joy/build'
   },
   test: {
     username: 'root',
@@ -33,7 +33,7 @@ config = {
     mongodb: `mongodb://${process.env.MONGO_USERNAME}:${
       process.env.MONGO_PASSWORD
     }@ds145563.mlab.com:45563/joy`,
-    STATIC_FILES: '/client/build'
+    STATIC_FILES: '/../client/build'
   }
 };
 
