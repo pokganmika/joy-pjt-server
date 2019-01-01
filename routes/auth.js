f15c60db1ecd03cd262195660646718668967c9a
tkhwang <tkhwang@gmail.com>
2019-01-01 10:25:59 +0900

    Change token delivery method for fixing bug

--- routes/auth.js
+++ routes/auth.js
@@ -185,32 +185,7 @@ router.get('/social/token', (req, res) => {
   // );
 
   res.redirect(`/?token=${token}`);
-
   // return res.json(token);
 });
 
 module.exports = router;
-
-/*
-router.get('/signup', function(req, res, next) {
-  res.send('/auth/signup');
-});
-
-router.get('/signin', function(req, res, next) {
-  res.send('/auth/signin');
-});
-
-router.get('/signout', function(req, res, next) {
-  // res.send('/auth/signout');
-  if (req.session.user) {
-    req.session.destroy(function(err) {
-      console.log('[+] /auth/signout : destroyed sessions and sign-out.');
-    });
-    res.redirect('/auth/signin');
-  } else {
-    res.redirect('/auth/signin');
-  }
-});
-
-module.exports = router;
-*/
