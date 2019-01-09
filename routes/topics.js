2f0341c2c1a4f0e37227b83e7163ab45fed29fa0
tkhwang <tkhwang@gmail.com>
2019-01-09 16:59:21 +0900

    Add user avatar for profile image

--- routes/topics.js
+++ routes/topics.js
@@ -4,6 +4,26 @@ var router = express.Router();
 const { Topic } = require('../models');
 
 router.get('/', async function(req, res, next) {
+  /*
+    let result = {};
+  var count = 0;
+  const topics = await Topic.findAll();
+  console.log('[+] //////////// length = ', topics.length);
+  topics.forEach(async topic => {
+    count++;
+    let counts = {};
+
+    const instructors = await topic.getInstructors();
+    counts['instructors'] = instructors.length;
+
+    const lectures = await topic.getLectures();
+    counts['lectures'] = lectures.length;
+
+    result[topic.name] = counts;
+    console.log(count, result);
+
+    if (count === topics.length) res.send(result);
+  */
   const topics = await Topic.findAll();
   res.send(topics);
 });
